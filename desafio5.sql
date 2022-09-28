d6c864dbfc5bbd99d565e07bea89f7c3b9fd77a7
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-09-28 10:31:45 -0300

    exercicio 5 - mostrando musicas mais tocadas

---
+++ desafio5.sql
@@ -0,0 +1,5 @@
+SELECT can.coluna_cancoes As cancoes, COUNT(can.coluna_cancoes) AS reproducoes FROM SpotifyClone.tabela_cancoes AS can
+INNER JOIN SpotifyClone.tabela_historico_de_reproducoes re
+ON can.cancao_id = re.coluna_historico_de_reproducoes
+GROUP BY can.coluna_cancoes
+ORDER BY reproducoes DESC LIMIT 2;
